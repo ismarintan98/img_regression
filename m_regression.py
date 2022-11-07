aa6ef81e29911d96638fcdd969f30d8d6b9df3c2
Marin <ismarintan98@gmail.com>
2022-11-07 12:38:32 +0700

    forward model

--- m_regression.py
+++ m_regression.py
@@ -7,21 +7,30 @@ class nn_layers:
         self.input_layer = np.zeros(input_size)
         self.hidden_layer = np.zeros(hidden_size)
         self.output_layer = np.zeros(hidden_size)
+        self.gradient = np.zeros(hidden_size)
 
         self.weights = np.random.rand(input_size,hidden_size)
 
         self.bias = np.random.rand(hidden_size)
 
         
-        
-        pass
 
     def forward(self,input):
         self.input_layer = input
         self.hidden_layer = np.matmul(self.input_layer,self.weights) + self.bias
-        self.output_layer = self.hidden_layer
+
+        #output layer sigmoid
+        self.output_layer = 1/(1+np.exp(-self.hidden_layer))
 
         return self.output_layer
+
+    def backward(self,learning_rate):
+        
+        #gradient sigmoid
+        self.gradient = self.output_layer*(1-self.output_layer)
+
+        return self.gradient
+
 
 
 
@@ -33,33 +42,18 @@ def main():
     banner = open("banner.txt","r")
     print(banner.read())
 
-    layer1 = nn_layers(2,2)
-    layer1.weights[0]  = [1,2]
-    layer1.weights[1]  = [3,4]
-    
-    layer1.bias[0] = 1
-    layer1.bias[1] = 2
-    
+    layer1 = nn_layers(2,12)
+    layer2 = nn_layers(12,1)
 
-    input = [1,2]
+    input = [0,1]
+    layer1.forward(input)
+    layer2.forward(layer1.output_layer)
 
-    
+    layer2.backward(0.1)
 
-    output = layer1.forward(input)
 
-    print("------- Calc -------")
-    print(np.shape(input))
-    print(np.shape(layer1.weights))
-    print(np.shape(layer1.bias))
-    print(np.shape(layer1.hidden_layer))
-    print(np.shape(layer1.output_layer))
-    print("------- Result -------")
-    print(output)
-    # print("input:", input)
-    # Calling the forward function in the nn_layers class.
-    # print("output:", output)
 
-    
+
 
 
 if __name__ == '__main__':
